61d074f3c72eb5658feb8764f98452f638b31cf7
Jhonny Lima <lima.jhonny95@gmail.com>
2025-06-07 23:21:58 -0300

    feat: integrate `llama-3.3` to the project

--- chatbot.py
+++ chatbot.py
@@ -1,20 +1,21 @@
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
 import os
 
 load_dotenv()
 api_key = os.getenv('GROQ_API_KEY')
+chat = ChatGroq(model='llama-3.3-70b-versatile')
 
 print('Welcome to the chatbot!')
 print('Type "exit" or press "X" to end the conversation.\n')
 
 messages = []
-def add_message(role, content):
+def append_message(role, content):
   messages.append({'role': role, 'content': content})
 
-def generate_response():
-  # This function would typically generate a response based on the messages.
-  # For demonstration, we'll just return a placeholder response.
-  return "This is a placeholder response."
+def generate_response(user_input):
+  response = chat.invoke(user_input)
+  return response.content
 
 while True: 
   user_input = input('You: ')
@@ -23,9 +24,9 @@ while True:
     print('Chatbot: Goodbye!\n')
     break
   
-  add_message('user', user_input)
-  response = generate_response()
-  add_message('assistant', response)
+  append_message('user', user_input)
+  response = generate_response(user_input)
+  append_message('assistant', response)
   print(f'ChatBot: {response}')
 
 print(f'Messages: {messages}\n')
