d0b01f9d82bd0136daa6534a7c8c8fb19791ec18
Jhonny Lima <lima.jhonny95@gmail.com>
2025-06-08 19:51:14 -0300

    refactor: create a template prompt and a chain

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,27 @@
+import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-import os
+from langchain.prompts import ChatPromptTemplate
 
 load_dotenv()
 api_key = os.getenv('GROQ_API_KEY')
 chat = ChatGroq(model='llama-3.3-70b-versatile')
 
+def append_message(role, content):
+  messages.append((role, content))
+
+def generate_response(messages):
+  chat_prompts = [('system', 'You are a friendly helpful assistant called Chatty.')]
+  chat_prompts += messages
+  template = ChatPromptTemplate.from_messages(chat_prompts)
+  chain = template | chat
+  response = chain.invoke({})
+  return response.content
+
 print('Welcome to the chatbot!')
 print('Type "exit" or press "X" to end the conversation.\n')
 
 messages = []
-def append_message(role, content):
-  messages.append({'role': role, 'content': content})
-
-def generate_response(user_input):
-  response = chat.invoke(user_input)
-  return response.content
 
 while True: 
   user_input = input('You: ')
@@ -25,8 +31,8 @@ while True:
     break
   
   append_message('user', user_input)
-  response = generate_response(user_input)
+  response = generate_response(messages)
   append_message('assistant', response)
-  print(f'ChatBot: {response}')
+  print(f'ChatBot: {response}\n')
 
 print(f'Messages: {messages}\n')
