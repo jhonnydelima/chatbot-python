1514016f473a0ce4ca038dd227d21622dc39b09c
Jhonny Lima <lima.jhonny95@gmail.com>
2025-06-09 00:11:31 -0300

    feat: load data/document from website

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,29 @@
 import os
 from dotenv import load_dotenv
+load_dotenv()
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_groq import ChatGroq
 from langchain.prompts import ChatPromptTemplate
 
-load_dotenv()
 api_key = os.getenv('GROQ_API_KEY')
 chat = ChatGroq(model='llama-3.3-70b-versatile')
+loader = WebBaseLoader('https://www.wikipedia.org')
+documents_list = loader.load()
+
+document = ''
+for doc in documents_list:
+  document += doc.page_content + '\n'
 
 def append_message(role, content):
   messages.append((role, content))
 
-def generate_response(messages):
-  chat_prompts = [('system', 'You are a friendly helpful assistant called Chatty.')]
-  chat_prompts += messages
-  template = ChatPromptTemplate.from_messages(chat_prompts)
+def generate_response(user_input):
+  template = ChatPromptTemplate.from_messages([
+    ('system', 'You are a friendly helpful assistant called Chatty. You have access to the following information to provide answers: {documents}'),
+    ('user', '{input}')
+  ])
   chain = template | chat
-  response = chain.invoke({})
+  response = chain.invoke({'documents': document, 'input': user_input})
   return response.content
 
 print('Welcome to the chatbot!')
@@ -31,7 +39,7 @@ while True:
     break
   
   append_message('user', user_input)
-  response = generate_response(messages)
+  response = generate_response(user_input)
   append_message('assistant', response)
   print(f'ChatBot: {response}\n')
 
